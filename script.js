0862fb0f71056ddd0a698d2926d7170602ba259d
Santiago <santimella11@gmail.com>
2026-02-08 06:10:09 -0300

    me olvide de hacer repositorios

---
+++ script.js
@@ -0,0 +1,781 @@
+let AUTOS = [
+  { modelo:"PEUGEOT 208",
+    pistas:[
+      "Peugeot (Francia)",
+      "Motores: 1.2, 1.6",
+      "Hatchback chico",
+      "Versiones: Allure, Feline, GT",
+      "Muy popular entre jóvenes y usado para ciudad",
+      "IMAGEN"
+    ]
+  },
+
+  { modelo:"PEUGEOT 2008",
+    pistas:[
+      "Peugeot (Francia)",
+      "Motores: 1.6, 1.6 THP",
+      "SUV compacta",
+      "Versiones: Allure, Feline, Sport",
+      "Muy vista en zonas urbanas por su tamaño cómodo",
+      "IMAGEN"
+    ]
+  },
+
+  { modelo:"RENAULT LOGAN",
+    pistas:[
+      "Renault (Francia)",
+      "Motor: 1.6",
+      "Sedán chico",
+      "Versiones: Life, Zen, Intens",
+      "Muy usado como auto familiar y en taxis por su espacio",
+      "IMAGEN"
+    ]
+  },
+
+  { modelo:"FIAT CRONOS",
+    pistas:[
+      "Fiat (Italia)",
+      "Motores: 1.3, 1.8",
+      "Sedán chico",
+      "Versiones: Drive, Precision, Stile",
+      "Uno de los autos más vendidos en Argentina en los últimos años",
+      "IMAGEN"
+    ]
+  },
+
+  { modelo:"TOYOTA COROLLA",
+    pistas:[
+      "Toyota (Japón)",
+      "Motores: 1.8, 2.0",
+      "Sedán mediano",
+      "Versiones: XLI, XEI, SEG",
+      "Muy confiable y común en rutas argentinas",
+      "IMAGEN"
+    ]
+  },
+
+  { modelo:"CHEVROLET ONIX",
+    pistas:[
+      "Chevrolet (EE.UU.)",
+      "Motores: 1.2, 1.4",
+      "Hatchback chico",
+      "Versiones: LT, LTZ, Premier",
+      "Muy popular por su consumo y equipamiento",
+      "IMAGEN"
+    ]
+  },
+
+  { modelo:"CITROËN C3 AIRCROSS",
+    pistas:[
+      "Citroën (Francia)",
+      "Motor: 1.6",
+      "SUV compacta",
+      "Versiones: Feel, Shine, Live",
+      "Reconocida por su comodidad y diseño alto",
+      "IMAGEN"
+    ]
+  },
+
+  { modelo:"TOYOTA HILUX",
+    pistas:[
+      "Toyota (Japón)",
+      "Motores: 2.4, 2.8",
+      "Pick-up mediana",
+      "Versiones: DX, SR, SRX",
+      "La pick-up más usada en el campo argentino",
+      "IMAGEN"
+    ]
+  },
+
+  { modelo:"JEEP CHEROKEE",
+    pistas:[
+      "Jeep (EE.UU.)",
+      "Motor: 3.7",
+      "SUV grande",
+      "Versiones: Sport, Limited, Overland",
+      "Muy robusta y típica camioneta familiar premium",
+      "IMAGEN"
+    ]
+  },
+
+  { modelo:"CITROËN C4",
+    pistas:[
+      "Citroën (Francia)",
+      "Motores: 1.6, 1.6 THP",
+      "Compacto / mediano",
+      "Versiones: C4 Lounge, Feel, Shine",
+      "Muy visto como auto familiar cómodo para ruta",
+      "IMAGEN"
+    ]
+  },
+
+  { modelo:"HONDA CIVIC",
+    pistas:[
+      "Honda (Japón)",
+      "Motores: 1.8, 2.0",
+      "Sedán mediano",
+      "Versiones: LX, EX, Touring",
+      "Conocido por su confiabilidad y estilo deportivo",
+      "IMAGEN"
+    ]
+  },
+
+  { modelo:"TOYOTA YARIS",
+    pistas:[
+      "Toyota (Japón)",
+      "Motores: 1.3, 1.5",
+      "Hatch o sedán subcompacto",
+      "Versiones: XS, XLS, GR-S",
+      "Muy usado en ciudad por su bajo consumo",
+      "IMAGEN"
+    ]
+  },
+
+  { modelo:"FORD FIESTA",
+    pistas:[
+      "Ford (EE.UU.)",
+      "Motor: 1.6",
+      "Hatchback chico",
+      "Versiones: S, SE, Titanium",
+      "Muy común en jóvenes y autos escuela",
+      "IMAGEN"
+    ]
+  },
+
+  { modelo:"FORD FOCUS",
+    pistas:[
+      "Ford (EE.UU.)",
+      "Motores: 1.6, 2.0",
+      "Hatch o sedán mediano",
+      "Versiones: S, SE, Titanium",
+      "Popular por su estabilidad y confort",
+      "IMAGEN"
+    ]
+  },
+
+  { modelo:"RENAULT SANDERO",
+    pistas:[
+      "Renault (Francia)",
+      "Motor: 1.6",
+      "Hatch amplio",
+      "Versiones: Authentique, Life, Iconic",
+      "Muy elegido como auto económico y familiar",
+      "IMAGEN"
+    ]
+  },
+
+  { modelo:"RENAULT STEPWAY",
+    pistas:[
+      "Renault (Francia)",
+      "Motor: 1.6",
+      "Hatch elevado",
+      "Versiones: Life, Iconic, Outsider",
+      "Muy popular por su estilo aventurero",
+      "IMAGEN"
+    ]
+  },
+
+  { modelo:"FORD RANGER",
+    pistas:[
+      "Ford (EE.UU.)",
+      "Motores: 2.2, 3.2",
+      "Pick-up mediana",
+      "Versiones: XL, XLS, Limited",
+      "Muy usada en campo y trabajos pesados",
+      "IMAGEN"
+    ]
+  },
+
+  { modelo:"VW AMAROK",
+    pistas:[
+      "Volkswagen (Alemania)",
+      "Motores: 2.0, 3.0 V6",
+      "Pick-up mediana",
+      "Versiones: Trendline, Highline, V6",
+      "Famosa por su versión V6 muy potente",
+      "IMAGEN"
+    ]
+  },
+
+  { modelo:"PEUGEOT 308",
+    pistas:[
+      "Peugeot (Francia)",
+      "Motores: 1.6, 1.6 THP",
+      "Hatchback mediano",
+      "Versiones: Allure, Feline, GT",
+      "Muy usado en ciudad con estética moderna",
+      "IMAGEN"
+    ]
+  },
+
+  { modelo:"RENAULT CLIO",
+    pistas:[
+      "Renault (Francia)",
+      "Motores: 1.2, 1.4, 1.6",
+      "Hatch chico",
+      "Versiones: Campus, Mio, Authentique",
+      "Popular entre jóvenes por su bajo costo",
+      "IMAGEN"
+    ]
+  },
+
+  { modelo:"RENAULT DUSTER",
+    pistas:[
+      "Renault (Francia)",
+      "Motores: 1.6, 2.0",
+      "SUV compacta",
+      "Versiones: Expression, Dynamique, Iconic",
+      "Muy usada en provincias y caminos malos",
+      "IMAGEN"
+    ]
+  },
+
+  { modelo:"FIAT SIENA",
+    pistas:[
+      "Fiat (Italia)",
+      "Motores: 1.4, 1.6",
+      "Sedán chico",
+      "Versiones: Fire, Attractive, Essence",
+      "Muy común como taxi o remis",
+      "IMAGEN"
+    ]
+  },
+
+  { modelo:"FORD KA",
+    pistas:[
+      "Ford (EE.UU.)",
+      "Motores: 1.0, 1.5",
+      "Hatch chico",
+      "Versiones: SE, SEL, Freestyle",
+      "Muy popular para uso urbano",
+      "IMAGEN"
+    ]
+  },
+
+  { modelo:"FORD ECOSPORT",
+    pistas:[
+      "Ford (EE.UU.)",
+      "Motores: 1.6, 2.0, 1.5 Dragon",
+      "SUV compacta",
+      "Versiones: S, SE, Titanium, Storm",
+      "Muy famosa por su rueda trasera",
+      "IMAGEN"
+    ]
+  },
+
+  { modelo:"TOYOTA ETIOS",
+    pistas:[
+      "Toyota (Japón)",
+      "Motor: 1.5",
+      "Hatch o sedán chico",
+      "Versiones: X, XLS, Platinum",
+      "Muy confiable y económico",
+      "IMAGEN"
+    ]
+  },
+
+  { modelo:"TOYOTA COROLLA CROSS",
+    pistas:[
+      "Toyota (Japón)",
+      "Motores: 2.0, 1.8 Hybrid",
+      "SUV compacta",
+      "Versiones: XLI, XEI, SEG, Hybrid",
+      "Muy popular en familias",
+      "IMAGEN"
+    ]
+  },
+
+  { modelo:"HONDA FIT",
+    pistas:[
+      "Honda (Japón)",
+      "Motor: 1.5",
+      "Hatch subcompacto",
+      "Versiones: DX, LX, EX",
+      "Muy famoso por su espacio interior",
+      "IMAGEN"
+    ]
+  },
+
+  { modelo:"HONDA HR-V",
+    pistas:[
+      "Honda (Japón)",
+      "Motor: 1.8",
+      "SUV compacta",
+      "Versiones: LX, EX, EXL",
+      "Muy valorada por su comodidad",
+      "IMAGEN"
+    ]
+  },
+
+  { modelo:"KIA RIO",
+    pistas:[
+      "Kia (Corea del Sur)",
+      "Motores: 1.4, 1.6",
+      "Hatch o sedán chico",
+      "Versiones: LX, EX, SX",
+      "Reconocido por su diseño moderno",
+      "IMAGEN"
+    ]
+  },
+
+  { modelo:"KIA CERATO",
+    pistas:[
+      "Kia (Corea del Sur)",
+      "Motores: 1.6, 2.0",
+      "Sedán mediano",
+      "Versiones: LX, EX, GT-Line",
+      "Elegido por su diseño y calidad",
+      "IMAGEN"
+    ]
+  },
+
+  { modelo:"JEEP RENEGADE",
+    pistas:[
+      "Jeep (EE.UU.)",
+      "Motores: 1.8, 1.4 T",
+      "SUV compacta",
+      "Versiones: Sport, Longitude, Trailhawk",
+      "Muy popular por su diseño cuadrado",
+      "IMAGEN"
+    ]
+  },
+
+  { modelo:"JEEP COMPASS",
+    pistas:[
+      "Jeep (EE.UU.)",
+      "Motores: 2.0, 2.4",
+      "SUV mediana",
+      "Versiones: Sport, Longitude, Limited",
+      "Elegida por su confort premium",
+      "IMAGEN"
+    ]
+  },
+
+  { modelo:"RENAULT KWID",
+    pistas:[
+      "Renault (Francia)",
+      "Motor: 1.0",
+      "Hatch subcompacto",
+      "Versiones: Life, Zen, Intens, Outsider",
+      "Muy económico y accesible",
+      "IMAGEN"
+    ]
+  },
+
+  { modelo:"CHEVROLET MERIVA",
+    pistas:[
+      "Chevrolet (EE.UU.)",
+      "Motor: 1.8",
+      "Monovolumen compacto",
+      "Versiones: GL, GLS, Premium, Easytronic",
+      "Muy práctica y vista entre 2005 y 2012",
+      "IMAGEN"
+    ]
+  },
+
+  { modelo:"AUDI A1",
+  pistas:[
+    "Audi (Alemania)",
+    "Motor: 1.4 Turbo / 1.0 Turbo",
+    "Hatch premium chico",
+    "Versiones: Attraction, Ambition",
+    "Muy popular entre jóvenes con autos premium",
+    "IMAGEN"
+  ]
+},
+
+{ modelo:"AUDI A3",
+  pistas:[
+    "Audi (Alemania)",
+    "Motor: 1.4 TFSI / 1.8 TFSI / 2.0 TFSI",
+    "Sedán / Hatch premium",
+    "Versiones: Attraction, Ambition, S-Line",
+    "Muy común en el segmento C premium",
+    "IMAGEN"
+  ]
+},
+
+{ modelo:"AUDI A4",
+  pistas:[
+    "Audi (Alemania)",
+    "Motor: 1.8 TFSI / 2.0 TFSI",
+    "Sedán mediano premium",
+    "Versiones: Advance, Ambition, S-Line",
+    "Típico sedán premium alemán",
+    "IMAGEN"
+  ]
+},
+
+{ modelo:"AUDI TT",
+  pistas:[
+    "Audi (Alemania)",
+    "Motor: 2.0 TFSI",
+    "Coupé deportiva",
+    "Versiones: Base, S-Line, TTS",
+    "Muy distintivo por su diseño redondeado",
+    "IMAGEN"
+  ]
+},
+
+{ modelo:"CHEVROLET S10",
+  pistas:[
+    "Chevrolet (EE.UU.)",
+    "Motor: 2.8 TurboDiésel",
+    "Pick-up mediana",
+    "Versiones: LS, LT, LTZ, High Country",
+    "Muy usada para trabajo y campo",
+    "IMAGEN"
+  ]
+},
+
+{ modelo:"CHEVROLET TRACKER",
+  pistas:[
+    "Chevrolet (EE.UU.)",
+    "Motor: 1.2 Turbo / 1.8 (versiones viejas)",
+    "SUV chica",
+    "Versiones: LT, LTZ, Premier",
+    "Muy vendida desde 2020 en adelante",
+    "IMAGEN"
+  ]
+},
+
+{ modelo:"CHEVROLET AGILE",
+  pistas:[
+    "Chevrolet (Brasil)",
+    "Motor: 1.4",
+    "Hatch chico",
+    "Versiones: LS, LT, LTZ",
+    "Muy visto entre 2010 y 2016",
+    "IMAGEN"
+  ]
+},
+
+{ modelo:"JEEP WRANGLER",
+  pistas:[
+    "Jeep (EE.UU.)",
+    "Motor: 3.6 V6",
+    "SUV 4x4 pura",
+    "Versiones: Sport, Sahara, Rubicon",
+    "Clásico jeep cuadrado para off-road duro",
+    "IMAGEN"
+  ]
+},
+
+{ modelo:"JEEP COMMANDER",
+  pistas:[
+    "Jeep (EE.UU.)",
+    "Motor: 1.3 Turbo / 2.0 TD",
+    "SUV mediana de 7 asientos",
+    "Versiones: Limited, Overland",
+    "Muy familiar y moderna",
+    "IMAGEN"
+  ]
+},
+
+{ modelo:"FIAT TORO",
+  pistas:[
+    "Fiat (Italia)",
+    "Motor: 2.0 TD / 1.8 / 1.3 Turbo",
+    "Pick-up mediana/compacta",
+    "Versiones: Freedom, Volcano, Ranch, Ultra",
+    "Muy popular por su estilo y confort",
+    "IMAGEN"
+  ]
+},
+
+{ modelo:"FIAT PALIO",
+  pistas:[
+    "Fiat (Italia)",
+    "Motor: 1.4 / 1.6",
+    "Hatch chico",
+    "Versiones: Fire, Attractive, Essence",
+    "Un clásico argentino y del Mercosur",
+    "IMAGEN"
+  ]
+},
+
+{ modelo:"FIAT SIENA",
+  pistas:[
+    "Fiat (Italia)",
+    "Motor: 1.4 / 1.6",
+    "Sedán chico",
+    "Versiones: Fire, Attractive, Essence",
+    "Usado como taxi en muchos países",
+    "IMAGEN"
+  ]
+},
+
+{ modelo:"FIAT FIORINO",
+  pistas:[
+    "Fiat (Italia)",
+    "Motor: 1.4",
+    "Furgón chico",
+    "Versiones: Fire, Hard Working",
+    "Muy usado para repartos",
+    "IMAGEN"
+  ]
+},
+
+{ modelo:"CITROËN BERLINGO",
+  pistas:[
+    "Citroën (Francia)",
+    "Motor: 1.6 HDI / 1.4",
+    "Furgón / Familiar",
+    "Versiones: Multispace, Furgón",
+    "Muy usada para carga y trabajo",
+    "IMAGEN"
+  ]
+},
+
+{ modelo:"CITROËN JUMPER",
+  pistas:[
+    "Citroën (Francia)",
+    "Motor: 2.3 / 2.2 HDI",
+    "Furgón grande",
+    "Versiones: Furgón L1/L2/L3",
+    "Clásica van de carga grande",
+    "IMAGEN"
+  ]
+},
+
+{ modelo:"HONDA CR-V",
+  pistas:[
+    "Honda (Japón)",
+    "Motor: 2.4 / 1.5 Turbo",
+    "SUV mediana",
+    "Versiones: LX, EX, EXL",
+    "Muy confiable y familiar",
+    "IMAGEN"
+  ]
+},
+
+{ modelo:"HONDA BR-V",
+  pistas:[
+    "Honda (Japón)",
+    "Motor: 1.5",
+    "SUV chica de 7 asientos",
+    "Versiones: LX, EX",
+    "Poco común pero muy práctica",
+    "IMAGEN"
+  ]
+},
+
+{ modelo:"HONDA CITY",
+  pistas:[
+    "Honda (Japón)",
+    "Motor: 1.5",
+    "Sedán chico",
+    "Versiones: LX, EX, EXL",
+    "Muy eficiente y confiable",
+    "IMAGEN"
+  ]
+},
+
+{ modelo:"HYUNDAI ACCENT",
+  pistas:[
+    "Hyundai (Corea del Sur)",
+    "Motor: 1.6",
+    "Sedán chico",
+    "Versiones: Base, Full",
+    "Muy común como auto urbano",
+    "IMAGEN"
+  ]
+},
+
+{ modelo:"HYUNDAI TUCSON",
+  pistas:[
+    "Hyundai (Corea del Sur)",
+    "Motor: 2.0 / 1.6 Turbo",
+    "SUV mediana",
+    "Versiones: GLS, Limited",
+    "Muy vista en Argentina",
+    "IMAGEN"
+  ]
+},
+
+{ modelo:"KIA PICANTO",
+  pistas:[
+    "Kia (Corea del Sur)",
+    "Motor: 1.0 / 1.2",
+    "Citycar",
+    "Versiones: Base, Full",
+    "Muy chico y económico",
+    "IMAGEN"
+  ]
+},
+
+{ modelo:"KIA CERATO",
+  pistas:[
+    "Kia (Corea del Sur)",
+    "Motor: 1.6 / 2.0",
+    "Sedán compacto",
+    "Versiones: LX, EX, SX",
+    "Muy buena relación precio/calidad",
+    "IMAGEN"
+  ]
+},
+
+{ modelo:"KIA SPORTAGE",
+  pistas:[
+    "Kia (Corea del Sur)",
+    "Motor: 2.0 / 1.6 Turbo",
+    "SUV mediana",
+    "Versiones: LX, EX, GT-Line",
+    "Muy popular en el Mercosur",
+    "IMAGEN"
+  ]
+},
+
+{ modelo:"PEUGEOT 408",
+  pistas:[
+    "Peugeot (Francia)",
+    "Motor: 2.0 / 1.6 Turbo",
+    "Sedán mediano",
+    "Versiones: Allure, Feline",
+    "Muy visto entre 2011 y 2020",
+    "IMAGEN"
+  ]
+},
+
+{ modelo:"PEUGEOT 5008",
+  pistas:[
+    "Peugeot (Francia)",
+    "Motor: 1.6 Turbo / 2.0 HDI",
+    "SUV grande de 7 asientos",
+    "Versiones: Allure, GT-Line, GT",
+    "Muy moderno y elegante",
+    "IMAGEN"
+  ]
+},
+
+{ modelo:"PEUGEOT EXPERT",
+  pistas:[
+    "Peugeot (Francia)",
+    "Motor: 1.6 HDI / 2.0 HDI",
+    "Furgón mediano",
+    "Versiones: Furgón, Premium",
+    "Muy usada para transporte",
+    "IMAGEN"
+  ]
+},
+
+{ modelo:"PEUGEOT 206",
+  pistas:[
+    "Peugeot (Francia)",
+    "Motor: 1.4 / 1.6",
+    "Hatch chico",
+    "Versiones: XR, XT, XTD, GTI",
+    "Muy famoso en Argentina",
+    "IMAGEN"
+  ]
+}
+
+  
+];
+
+
+// ==========================
+
+let autoActual = null;
+let pistaActual = 0;
+
+const pistaEls = [...document.querySelectorAll(".pista")];
+const input = document.getElementById("adivinador");
+const sugerenciasWrap = document.getElementById("sugerencias");
+const btnSiguiente = document.getElementById("btnSiguiente");
+const btnAdivinar = document.getElementById("btnAdivinar");
+const btnReiniciar = document.getElementById("btnReiniciar");
+const resultadoDiv = document.getElementById("resultado");
+const contadorSpan = document.getElementById("contador");
+
+const modelos = AUTOS.map(a => a.modelo);
+
+/* ========================== */
+
+function elegirAutoAleatorio(){
+  autoActual = AUTOS[Math.floor(Math.random()*AUTOS.length)];
+  pistaActual = 0;
+  pistaEls.forEach(el => { el.style.display="none"; el.innerHTML=""; });
+  mostrarPista(0);
+  resultadoDiv.style.display="none";
+  input.value="";
+  sugerenciasWrap.innerHTML="";
+  btnSiguiente.disabled=false;
+  btnAdivinar.disabled=false;
+  contadorSpan.textContent="1";
+}
+
+function mostrarPista(i){
+  const el = pistaEls[i];
+  el.style.display = "block";
+  el.classList.remove("animar"); 
+  void el.offsetWidth;
+  el.classList.add("animar");  
+
+  if (i === 5) {
+    const nombre = autoActual.modelo.replace(/\s+/g, "").toUpperCase();
+    el.innerHTML = `
+      PISTA 6:<br>
+      <img src="imagenes/${nombre}.jpeg" style="max-width:160px; border-radius:12px;">
+    `;
+    return;
+  }
+
+  el.textContent = "PISTA " + (i+1) + ": " + autoActual.pistas[i];
+}
+
+btnSiguiente.onclick = ()=>{
+  if(pistaActual < 5){
+    pistaActual++;
+    mostrarPista(pistaActual);
+    contadorSpan.textContent = (pistaActual+1);
+  }
+  if(pistaActual === 5) btnSiguiente.disabled = true;
+};
+
+btnAdivinar.onclick = () => {
+  resultadoDiv.style.display = "block";
+  resultadoDiv.classList.remove("correcto", "incorrecto");
+
+  const txt = input.value.trim().toLowerCase();
+  const correcto = autoActual.modelo.toLowerCase();
+
+  if(txt === correcto){
+    resultadoDiv.classList.add("correcto");
+    resultadoDiv.textContent = "¡Correcto! Era " + autoActual.modelo;
+  } else {
+    resultadoDiv.classList.add("incorrecto");
+    resultadoDiv.textContent = "Incorrecto. Era " + autoActual.modelo;
+  }
+
+  btnSiguiente.disabled = true;
+  btnAdivinar.disabled = true;
+};
+
+
+btnReiniciar.onclick = elegirAutoAleatorio;
+
+/* Autocompletado */
+input.addEventListener("input", ()=>{
+  const q = input.value.toLowerCase();
+  sugerenciasWrap.innerHTML="";
+  if(!q) return;
+
+  modelos.filter(m=>m.toLowerCase().includes(q)).slice(0,6).forEach(m=>{
+    const div=document.createElement("div");
+    div.className="sugerencia";
+    div.textContent=m.toUpperCase();
+    div.onclick=()=>{ input.value=m; sugerenciasWrap.innerHTML=""; };
+    sugerenciasWrap.appendChild(div);
+  });
+});
+
+/* Enter = adivinar */
+input.addEventListener("keydown",(e)=>{
+  if(e.key==="Enter"){
+    e.preventDefault();
+    btnAdivinar.click();
+  }
+});
+
+window.onload = elegirAutoAleatorio;
